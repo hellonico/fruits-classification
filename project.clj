0ae607a0c6c5548efa44161b01ba27918ee1669c
Nicolas Modrzyk <hellonico@gmail.com>
2017-06-27 17:29:48 +0900

    cleaning up dependencies

--- project.clj
+++ project.clj
@@ -1,17 +1,21 @@
-(defproject fruits-classification "0.1-SNAPSHOT"
+(defproject fruits-classification "0.1"
   :description "An example of using experiment/classification on mnist."
+  :repositories {"hellonico"   {:url "http://hellonico.tokyo:8081/repository/hellonico/"}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [thinktopic/experiment "0.9.9"]
                  [org.clojure/tools.cli "0.3.5"]
 
                  ; to manipulate images
-                 [thinktopic/think.image "0.4.8"]
+                 [thinktopic/think.image "0.4.8" ]
+                 [org.hellonico/imgscalr-lib "4.3"]
+                 [org.hellonico/sizing "0.1.0"]
                  ; [net.mikera/imagez "0.12.0"]
                  ;;If you need cuda 8...
                  [org.bytedeco.javacpp-presets/cuda "8.0-1.2"]
                  ;;If you need cuda 7.5...
                  ;;[org.bytedeco.javacpp-presets/cuda "7.5-1.2"]
                  ]
+  :exclusions [org.imgscalr/imgscalr-lib]
 
   ; :main mnist-classification.main
   ; :aot [mnist-classification.main]
